89483fe79660c8bdd2677cef04a70c46fd47661e
Rabindranath <ferreiralox@gmail.com>
2018-06-05 23:37:31 -0400

    add Model Registrado to admin, and import model

--- boletin/admin.py
+++ boletin/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+# namespace.models import nombre_modelo
+from .models import Registrado
+
+# registrado el modelo Registrado dentro del admin
+admin.site.Register(Registrado)
--- boletin/models.py
+++ boletin/models.py
@@ -5,12 +5,18 @@ from django.db import models
 
 # Create your models here
 
+# IMPORTANTE LOS NOMBRE DE LOS MODELOS SIEMPRE VAN EN SINGULAR
+
 class Registrado(models.Model):
     # tamaño maximo del campo max_lenght
-    nombre = models.CharField(max_length=100, ${blank=True, null=True})
+    nombre = models.CharField(max_length=100, blank=True, null=True})
     email = models.EmailField()
     timestap = models.DateTimeField( auto_now_add = True, auto_now = False)
 
     def __unicode__(self):
         #python 2
-        return self.email
+        return self.nombre
+    
+    def __str__(self):
+        #python 3
+        return self.nombre
