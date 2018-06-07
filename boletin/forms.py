1da876fae97e95ae6444f474f7244ca1e8572e2d
Rabindranath <ferreiralox@gmail.com>
2018-06-07 00:45:45 -0400

    add form with method POST or None, use required un class form

--- boletin/forms.py
+++ boletin/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class RegForm(forms.Form):
-    nombre = forms.CharField(max_length=255)
-    edad = forms.IntegerField(max_value = 100 , min_value = 1 )
+    # si se deja required = false el campo deja de ser obligatorio, y sin colocar POST or None en el views.py
+    nombre = forms.CharField(max_length=255 , required = False)
+    edad = forms.IntegerField(max_value = 100 , min_value = 1 )#, required = False)
--- boletin/views.py
+++ boletin/views.py
@@ -11,6 +11,10 @@ from .forms import RegForm
 
 # Create your views here.
 def home(request):
-    form = RegForm()
+    #si agregamos request.POST, le indicamos en el form q el metodo el POST y procesaremos la data como tal
+    #si se agrega el None, se desactivan los campos obligatorios
+    form = RegForm(request.POST or None)
+    #Operaciones que podemos ver o realizarle al form se pueden ver con print(dir(form)) en la terminal del servidor de djago
+    #print(dir(form))
     context = { "form" : form }
     return render(request,'home.html',context)
