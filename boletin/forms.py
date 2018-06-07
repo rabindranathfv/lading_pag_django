924e16159e67bd8a2256691e32e43fac7090e9d1
Rabindranath <ferreiralox@gmail.com>
2018-06-07 01:12:00 -0400

    using methods for forms like cleaned_data, get in the views and editing the form.py

--- boletin/forms.py
+++ boletin/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class RegForm(forms.Form):
     # si se deja required = false el campo deja de ser obligatorio, y sin colocar POST or None en el views.py
     nombre = forms.CharField(max_length=255 , required = False)
-    edad = forms.IntegerField(max_value = 100 , min_value = 1 )#, required = False)
+    email = forms.EmailField(required = True)
--- boletin/views.py
+++ boletin/views.py
@@ -16,5 +16,14 @@ def home(request):
     form = RegForm(request.POST or None)
     #Operaciones que podemos ver o realizarle al form se pueden ver con print(dir(form)) en la terminal del servidor de djago
     #print(dir(form))
+    if form.is_valid():
+        # almacena toda la data del form en un diccionario usando el metodo cleaned_data
+        data_form = form.cleaned_data
+        #metodo get("nombre_campo_class_form") para traer el contenido
+        print data_form.get("nombre")
+        print data_form.get("email")
+
+    #limpiar el form
+    #form.clean_fields
     context = { "form" : form }
     return render(request,'home.html',context)
