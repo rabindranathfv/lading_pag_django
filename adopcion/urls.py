e730a34b437c40a715aaf0b6a0525b649a56b554
Rabindranath <ferreiralox@gmail.com>
2018-06-28 01:36:21 -0400

    add urls.py in mascota and adopcion, update requirements.txt

--- adopcion/urls.py
+++ adopcion/urls.py
@@ -21,13 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('mascota.urls',namespace='mascota')),
-    url(r'^', include('adopcion.urls',namespace='adopcion')),
+    
 
 ]
-
-# add config for link styles
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
