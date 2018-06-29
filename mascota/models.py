eb267e64d4c3d76cebfbd02436bb28a43a1f8715
Rabindranath <ferreiralox@gmail.com>
2018-06-28 23:54:49 -0400

    model mascota, vacuna and persona....migrations and relationships

--- mascota/models.py
+++ mascota/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=70)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    domicilio = models.TextField()
