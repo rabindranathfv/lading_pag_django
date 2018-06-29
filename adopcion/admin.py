bcd6d36fe2a08a2fe73925a02c034f9f06edd183
Rabindranath <ferreiralox@gmail.com>
2018-06-28 23:56:45 -0400

    register models in admin.py on app adopcion,mascota

--- adopcion/admin.py
+++ adopcion/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Persona
+# import models for register in admin
+from adopcion.models import Persona
 
 # Register your models here.
 admin.site.register(Persona)
--- mascota/admin.py
+++ mascota/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Vacuna,Mascota
+# import models for register in admin
+from mascota.models import Vacuna,Mascota
 
 # Register your models here.
-admin.site.register(Vacuna,Mascota)
+admin.site.register(Vacuna)
+admin.site.register(Persona)
