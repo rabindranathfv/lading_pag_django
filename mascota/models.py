b482c2941cb2cee7c5515a48db54df6f17234144
Rabindranath <ferreiralox@gmail.com>
2018-07-03 22:16:27 -0400

    fix model Mascota and migrations

--- mascota/models.py
+++ mascota/models.py
@@ -13,13 +13,13 @@ class Vacuna(models.Model):
 
 
 class Mascota(models.Model):
-    folio = models.CharField(max_length=10, primary_key=True)
+    
     nombre = models.CharField(max_length=50)
     sexo = models.CharField(max_length=10)
     edad_aproximada = models.IntegerField()
     fecha_rescate = models.DateField()
     persona = models.ForeignKey('adopcion.Persona',blank=True, null=True, on_delete=models.CASCADE)
-    vacuna = models.ManyToManyField(Vacuna)
+    vacuna = models.ManyToManyField(Vacuna,blank=True)
 
     def __unicode__(self):
         return self.nombre
