f767b4721c6c8430501662e16f6795f1c47114df
Rabindranath <ferreiralox@gmail.com>
2018-07-04 01:19:49 -0400

    fix import to urls at views.py in adopcion app

--- adopcion/urls.py
+++ adopcion/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .models import Persona
+
 
 urlpatterns = [
     
--- adopcion/views.py
+++ adopcion/views.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Persona
 # Create your views here.
